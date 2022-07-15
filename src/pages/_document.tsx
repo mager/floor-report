1e845f520b65c1fc0a83fedd42c71b370324f72a
Mager <andrew.mager@gmail.com>
2022-07-15 10:33:58 -0500

    Add apple-touch-icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,11 @@ class MyDocument extends Document<{stylesheets: Sheet[]}> {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
